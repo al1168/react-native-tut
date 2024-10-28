a01b4ccdb83464537c55b6f6bf767e0931fe3171
al1168 <business.aldenlu@gmail.com>
2024-10-28 16:36:36 -0400

    inital commit

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems:"center",
+    backgroundColor:"yellow",
+    flex: 1,
+    justifyContent:"center",
+  }
+});

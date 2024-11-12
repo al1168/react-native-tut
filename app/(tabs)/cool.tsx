d512c2f9b493b3bf5d7283cedc1016f61b7d8ab4
al1168 <business.aldenlu@gmail.com>
2024-11-11 21:02:35 -0500

    working app + starting camera integration

--- app/(tabs)/cool.tsx
+++ app/(tabs)/cool.tsx
@@ -1,7 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { useCameraPermission } from "react-native-vision-camera";
 
 const cool = () => {
+  const { hasPermission } = useCameraPermission()
+  if (!hasPermission) return null
   return (
     <View>
       <Text>cooltab no lie</Text>
